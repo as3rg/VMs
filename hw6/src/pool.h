ea0172560b528919044a9e4b66f7a25eaa16b569
as3rg <sergeev0xEF@gmail.com>
2026-04-13 23:17:57 +0300

    hw6: Fixes
    - Added a guard at the end of the stack

--- hw6/src/pool.h
+++ hw6/src/pool.h
@@ -19,8 +19,12 @@ class Pool {
   size_t size;
   static constexpr size_t PAGE = 4096;
 
+  static constexpr size_t pgcnt(size_t max_elems) {
+    return (max_elems * sizeof(T) + PAGE - 1) & ~(PAGE - 1);
+  }
+
 public:
-  Pool(size_t max_elems) : size((max_elems * sizeof(T) + PAGE - 1) & ~(PAGE - 1)) {
+  Pool(size_t max_elems) : size(pgcnt(max_elems) + PAGE) {
     ptr = mmap(NULL, size,
                PROT_READ | PROT_WRITE,
                MAP_PRIVATE | MAP_ANONYMOUS,
@@ -28,6 +32,11 @@ public:
     if (ptr == MAP_FAILED) {
       throw std::runtime_error("Failed to allocate memory");
     }
+
+    if (mprotect(ptr, PAGE, PROT_NONE)) {
+      throw std::runtime_error("Failed to add a guard");
+    }
+
     cur = reinterpret_cast<T*>(reinterpret_cast<char *>(ptr) + size);
     dbg("Mapped memory at %lx\n", reinterpret_cast<size_t>(cur));
   }
